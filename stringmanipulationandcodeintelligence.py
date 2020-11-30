88a34083731bf75e0cd80b7846e73153d30b7511
ezikechris <christopherezikeu2@gmail.com>
2020-11-30 10:33:28 +0100

    initial commit

---
+++ stringmanipulationandcodeintelligence.py
@@ -0,0 +1,19 @@
+print("helloworld\nhelloworld") 
+
+# CONCATINATING STRINGS # MEANS COMBINING STRINGS IN ONE WORD ATTACHING TWO STRINGS TOGETHER #
+
+
+print("hello " + " " +" CHRIS")
+
+# STRINGS AS STRINGS AS CONECTED CHARACTERS 
+
+# String concatenatio is simply taking does single strings and merging them into one string #
+
+
+# indentation error always start at the begining of the line #
+
+
+print("string manipulation")
+print("string concatenation is done with a" + " + sign")
+print("hello world\nhello world")
+print("new lines can be created with a backslash or n\nesther")
